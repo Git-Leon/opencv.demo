b2a690b556a9a918fc045bcb3a057ae1f1ee9f72
Leon Hunter <leonhunter@Leons-MacBook-Pro-2.local>
2019-05-11 22:58:05 -0400

    almost worked!

--- src/main/java/api/LegacyMainApplication.java
+++ src/main/java/api/LegacyMainApplication.java
@@ -12,67 +12,12 @@ import static org.bytedeco.javacpp.opencv_objdetect.CascadeClassifier;
 public class LegacyMainApplication {
 
     public static void main(String[] args) throws Exception {
-        FrontFaceClassifier frontFaceClassifier = new FrontFaceClassifier();
-        CascadeClassifier classifier = frontFaceClassifier.getClassifier();
-        FrameGrabber grabber = new FrameGrabberWrapper();
+        FrameGrabberWrapper grabber = new FrameGrabberWrapper();
         CanvasFrame frame = new CanvasFrame("Some Title", CanvasFrame.getDefaultGamma() / grabber.getGamma());
         FrameConverterWrapper converter = new FrameConverterWrapper();
 
         grabber.start();
-
-        Mat grabbedImage = converter.convert(grabber.grab());
-        Mat rotatedImage = grabbedImage.clone();
-        int height = grabbedImage.rows();
-        int width = grabbedImage.cols();
-
-        FrameRecorder recorder = new FrameRecorderWrapper(grabbedImage);
-        recorder.start();
-        Rotator3D rotator3D = new Rotator3D();
-        rotator3D.rotate(grabbedImage);
-        // We can allocate native arrays using constructors taking an integer as argument.
-        Point hatPoints = new Point(3);
-        Mat grayImage = new Mat(height, width, CV_8UC1);
-        while (frame.isVisible() && (grabbedImage = converter.convert(grabber.grab())) != null) {
-            // Let's try to detect some faces! but we need a grayscale image...
-            cvtColor(grabbedImage, grayImage, CV_BGR2GRAY);
-            RectVector faces = new RectVector();
-            classifier.detectMultiScale(grayImage, faces);
-
-            long total = faces.size();
-            for (long i = 0; i < total; i++) {
-                Rect r = faces.get(i);
-                int x = r.x(), y = r.y(), w = r.width(), h = r.height();
-                rectangle(grabbedImage, new Point(x, y), new Point(x + w, y + h), Scalar.RED, 1, CV_AA, 0);
-
-                // To access or pass as argument the elements of a native array, call position() before.
-                hatPoints.position(0).x(x - w / 10).y(y - h / 10);
-                hatPoints.position(1).x(x + w * 11 / 10).y(y - h / 10);
-                hatPoints.position(2).x(x + w / 2).y(y - h / 2);
-                fillConvexPoly(grabbedImage, hatPoints.position(0), 3, Scalar.GREEN, CV_AA, 0);
-            }
-
-            // Let's find some contours! but first some thresholding...
-            threshold(grayImage, grayImage, 64, 255, CV_THRESH_BINARY);
-
-            // To check if an output argument is null we may call either isNull() or equals(null).
-            MatVector contours = new MatVector();
-            findContours(grayImage, contours, CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
-            long n = contours.size();
-            for (long i = 0; i < n; i++) {
-                Mat contour = contours.get(i);
-                Mat points = new Mat();
-                approxPolyDP(contour, points, arcLength(contour, true) * 0.02, true);
-                drawContours(grabbedImage, new MatVector(points), -1, Scalar.BLUE);
-            }
-
-            warpPerspective(grabbedImage, rotatedImage, rotator3D.getRandomR(), rotatedImage.size());
-
-            Frame rotatedFrame = converter.convert(rotatedImage);
-            frame.showImage(rotatedFrame);
-            recorder.record(rotatedFrame);
-        }
-        frame.dispose();
-        recorder.stop();
-        grabber.stop();
+        FaceDetector faceDetector = new FaceDetector(grabber, converter, frame);
+        faceDetector.detect();
     }
 }
